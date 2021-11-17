49baa7ab739e51d0699c7fe9ab43f07a3b52aaf0
minimumw <wages95@gmail.com>
2021-11-17 14:06:57 -0500

    added ability to create grid of x number of squares from user input

--- script.js
+++ script.js
@@ -3,13 +3,16 @@
 // Query Selectors
 const canvas = document.querySelector(".canvas");
 const button = document.querySelector("#reset-btn");
+const canvasBlocks = document.querySelector(".canvas-block");
 
 // Set up canvas (user input and creating divs)
 const canvasSetup = function () {
   let divs = parseInt(prompt("How many squares per side (MAX: 100)"));
-  const div = `<div class="canvas-block"></div>`;
+  const divInsert = `<div class="canvas-block"></div>`;
+  canvas.style.setProperty("--grid-rows", divs);
+  canvas.style.setProperty("--grid-cols", divs);
   for (let i = 0; i < divs * divs; i++) {
-    canvas.insertAdjacentHTML("afterbegin", div);
+    canvas.insertAdjacentHTML("afterbegin", divInsert);
   }
 };
 
