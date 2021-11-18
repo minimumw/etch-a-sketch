627b4c10e976e2503aa1e79099bdfe441b706f07
minimumw <wages95@gmail.com>
2021-11-18 11:42:12 -0500

    got block hover event and reset button events setup and functioning; slight css tweaks

--- script.js
+++ script.js
@@ -2,18 +2,37 @@
 
 // Query Selectors
 const canvas = document.querySelector(".canvas");
-const button = document.querySelector("#reset-btn");
-const canvasBlocks = document.querySelector(".canvas-block");
+const resetBtn = document.getElementById("reset-btn");
 
 // Set up canvas (user input and creating divs)
 const canvasSetup = function () {
-  let divs = parseInt(prompt("How many squares per side (MAX: 100)"));
+  let blocks = parseInt(prompt("How many squares per side (MAX: 100)"));
   const divInsert = `<div class="canvas-block"></div>`;
-  canvas.style.setProperty("--grid-rows", divs);
-  canvas.style.setProperty("--grid-cols", divs);
-  for (let i = 0; i < divs * divs; i++) {
+  canvas.style.setProperty("--grid-rows", blocks);
+  canvas.style.setProperty("--grid-cols", blocks);
+  for (let i = 0; i < blocks * blocks; i++) {
     canvas.insertAdjacentHTML("afterbegin", divInsert);
   }
 };
-
 canvasSetup();
+
+const clearCanvas = function () {
+  canvas.innerHTML = "";
+  canvasSetup();
+  blockListener();
+};
+
+// canvas.addEventListener("mouseenter", function (e) {
+//   if (e.target.classList.contains("canvas-block")) console.log("hi");
+// });
+
+const blockListener = function () {
+  document.querySelectorAll(".canvas-block").forEach((el) => {
+    el.addEventListener("mouseenter", function () {
+      el.style.backgroundColor = "black";
+    });
+  });
+};
+blockListener();
+
+resetBtn.addEventListener("click", clearCanvas);
